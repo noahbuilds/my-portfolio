7c3efd7269d1d318023598d3d79702ca45ab3800
ed-knowah <myemail>
2022-10-23 08:47:46 +0100

    refactor: memoji

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,18 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  showMobileNav: boolean= false
-  constructor() { }
+  showMobileNav: boolean = false;
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleMobileMenu() {
+    this.showMobileNav = !this.showMobileNav;
   }
-
-  toggleMobileMenu(){
-    this.showMobileNav = !this.showMobileNav
-    console.log("Open Mobile menu yeah yeah")
-  }
-
 }
