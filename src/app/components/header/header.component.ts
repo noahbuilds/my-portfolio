795d514792e29f390baaa6b5ebb685da0ecac5cc
Noah Edet <edetnoah@gmail.com>
2026-08-29 19:02:23 +0100

    feat: update resume link, hero title, and fix calendar modal

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -16,7 +16,7 @@ export class HeaderComponent implements OnInit {
     // {name:'Projects', link:'/#projects'},
     {
       name: 'Résumé',
-      link: 'https://drive.google.com/file/d/1meRpemHSgLGWALHQZXW_UTQCMsH9D5z4/view?usp=sharing',
+      link: 'https://drive.google.com/file/d/1p1tXvpCORQZwWgoExFD-esIivLIW5GrJ/view?usp=sharing',
     },
   ];
   constructor(private router: Router) {}
